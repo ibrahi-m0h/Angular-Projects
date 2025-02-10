b59687cfab93298fb9beed69b771c1d5d8e936bf
M. Ibrahim Israr <israr.m.ibrahim@gmail.com>
2025-02-10 12:08:33 -0800

    Add styles to header

--- first-ng-app/src/app/components/header/header.component.ts
+++ first-ng-app/src/app/components/header/header.component.ts
@@ -7,5 +7,5 @@ import { Component, signal } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  title = signal('Angular app Header')
+  title = signal('My First Angular App')
 }
