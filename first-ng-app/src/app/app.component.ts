7254cada5ebb86e536a0fcb05d72ca1918bb5c5c
Ibrahim Israr <israr.m.ibrahim@gmail.com>
2025-02-11 17:16:04 -0800

    Use app router to route user to home and todo pages

--- first-ng-app/src/app/app.component.ts
+++ first-ng-app/src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './home/home.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent],
   template: `
     <app-header />
     <main>
-      <app-home />
+      <router-outlet />
     </main>
   `,
   styles: [`
